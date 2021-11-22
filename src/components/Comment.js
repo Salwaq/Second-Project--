d5e69b40f178fddf9c35ea4b04b9166250cf32fa
salwa alqurashi <92172130+Salwaq@users.noreply.github.com>
2021-11-22 15:12:32 +0300

    correct edit

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,15 +1,27 @@
-import { useContext } from "react"
-import { Card, Button } from "react-bootstrap"
+import { useContext, useState } from "react"
+import { Card, Button, Col, Image, Form, Modal } from "react-bootstrap"
 import MarvilContext from "../utils/MarvilContext"
-import EditModal from "./EditModal"
+
 function Comment(props) {
   const { comment, inProfile } = props
-  const { deleteComment, handleOpen } = useContext(MarvilContext)
+  const { confirmComment } = useContext(MarvilContext)
+  const [show, setShow] = useState(false)
+  const handleOpen = () => {
+    setShow(true)
+  }
+  const handleClose = () => {
+    setShow(false)
+  }
+
+  const { deleteComment } = useContext(MarvilContext)
 
   return (
-    <div className="comment">
-      <Card className="mb-2 mx-2 w-100">
+    <div>
+      <Card className="mb-2 mx-2 w-100" xs={6} md={4}>
         <Card.Header>
+          <Col xs={6} md={4}>
+            <Image src={comment._user.photo} height={50} roundedCircle />
+          </Col>
           {comment._user.firstName} {comment._user.lastName}
         </Card.Header>
         <Card.Body>
@@ -19,7 +31,7 @@ function Comment(props) {
         <div className="mb-2 mx-2 ">
           {inProfile ? (
             <>
-              <Button variant="outline-success" id={comment._id} onClick={handleOpen}>
+              <Button variant="outline-success" onClick={handleOpen}>
                 Edit
               </Button>{" "}
               <></>
@@ -32,7 +44,30 @@ function Comment(props) {
       </Card>
 
       {/* ---------------------------------------------------------------------- */}
-      <EditModal comment={comment} commentId={comment._id} />
+      <>
+        <Modal size="sm" show={show} onHide={handleClose} aria-labelledby="example-modal-sizes-title-sm">
+          <Form onSubmit={e => confirmComment(e, comment._id)}>
+            <Modal.Header closeButton></Modal.Header>
+            <Card>
+              <Card.Header>Edit Comment:</Card.Header>
+              <Card.Body>
+                <Card.Title>Name</Card.Title>
+                <input name="title" type="text" defaultValue={comment.title} />
+                <Card.Text>Description </Card.Text>
+                <textarea type="textarea" rows="5" name="description" defaultValue={comment.description} />
+              </Card.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" type="submit" onClick={handleClose}>
+                  Confirm
+                </Button>
+              </Modal.Footer>
+            </Card>
+          </Form>
+        </Modal>
+      </>
     </div>
   )
 }
